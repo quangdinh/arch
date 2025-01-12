8d0694cfe2b817cead71d5cafff321ca5283a143
Quang Dinh Le <lequangdinh@gmail.com>
2025-01-12 11:16:03 +0100

    Terminate user on logout

--- after_install/hyprland/wofipowermenu.py
+++ after_install/hyprland/wofipowermenu.py
@@ -12,7 +12,7 @@ def run_menu():
   )
 
   actions = (
-    "loginctl terminate-session $XDG_SESSION_ID",
+    "loginctl terminate-user \"\"",
     "systemctl suspend",
     "systemctl hibernate",
     "systemctl reboot",
