b81903eeada0e703fcca5ad795f07f8b9aa6448c
Quang Dinh Le <lequangdinh@gmail.com>
2025-01-14 22:32:34 +0100

    Kanshi & more packages

--- after_install/carbon-apps.sh
+++ after_install/carbon-apps.sh
@@ -3,5 +3,7 @@
 set -e
 yay -Syu --noconfirm rbenv ruby-build 1password mongodb-compass \
   postman-bin slack-desktop-wayland google-chrome \
-  gita kubectl k9s fnm-bin oh-my-posh-bin
-  sudo sed -i -E 's/Exec=(.*) mongodb-compass %U$/Exec=\1 mongodb-compass --ignore-additional-command-line-flags --password-store="gnome-libsecret" %U/g' /usr/share/applications/mongodb-compass.desktop
+  gita kubectl k9s fnm-bin oh-my-posh-bin obsidian
+
+
+sudo sed -i -E 's/Exec=(.*) mongodb-compass %U$/Exec=\1 mongodb-compass --ignore-additional-command-line-flags --password-store="gnome-libsecret" %U/g' /usr/share/applications/mongodb-compass.desktop
--- after_install/hyprland.sh
+++ after_install/hyprland.sh
@@ -7,12 +7,18 @@ sudo pacman -S --noconfirm hyprland hyprpaper hyprlock hypridle waybar wofi ghos
   xdg-user-dirs-gtk imv zathura zathura-pdf-poppler mpv xdg-desktop-portal-gtk \
   xdg-desktop-portal-hyprland swaync ly slurp grim wl-clipboard libnotify jq \
   swappy polkit-gnome ttf-dejavu noto-fonts noto-fonts-extra noto-fonts-emoji \
-  ncmpcpp mpd mpc nm-connection-editor brightnessctl
+  ncmpcpp mpd mpc nm-connection-editor brightnessctl hyprpicker hyprsunset \
+  kanshi
+
+yay -S --noconfirm overskride-bin
+
 sudo ./gnome_keyring.py
 sudo rm /usr/share/wayland-sessions/hyprland-uwsm.desktop
 sudo sed -i -e 's/Exec=.*$/Exec=uwsm start \&>> \/dev\/null -- Hyprland \&>> \/dev\/null/g' /usr/share/wayland-sessions/hyprland.desktop
 sudo systemctl enable ly.service
 systemctl --user enable waybar.service
+sudo rm -rf /usr/share/dbus-1/accessibility-services/org.a11y.*
+sudo rm -rf /usr/share/dbus-1/services/org.a11y.*
 sudo ./hide_system_apps.sh
 ./yazi.sh
 ./imv.sh
