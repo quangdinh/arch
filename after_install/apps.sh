192da7a4587eb804e7e887d5598c7174fc47dfca
Quang Dinh Le <lequangdinh@gmail.com>
2025-02-04 09:44:40 +0100

    Switched to zen browser

--- after_install/apps.sh
+++ after_install/apps.sh
@@ -2,9 +2,10 @@
 
 set -e
 yay -Syu --noconfirm rbenv ruby-build 1password mongodb-compass \
-  postman-bin slack-desktop-wayland google-chrome \
+  postman-bin slack-desktop-wayland zen-browser-bin \
   gita kubectl k9s fnm-bin oh-my-posh-bin obsidian
 
 
+fnm install --lts
 sudo sed -i -E 's/Exec=(.*) mongodb-compass %U$/Exec=\1 mongodb-compass --ignore-additional-command-line-flags --password-store="gnome-libsecret" %U/g' /usr/share/applications/mongodb-compass.desktop
 sudo sed -i -E 's/Exec=(.*)%U/Exec=env ELECTRON_OZONE_PLATFORM_HINT= \1%U/g' /usr/share/applications/1password.desktop
