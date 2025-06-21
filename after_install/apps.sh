c029de90151b9c95f05f54480645cc8a2b036f77
Quang Dinh Le <lequangdinh@gmail.com>
2025-06-21 22:17:41 +0200

    Update

--- after_install/apps.sh
+++ after_install/apps.sh
@@ -9,4 +9,5 @@ yay -S --noconfirm --mflags "--nocheck" mongodb-compass-bin
 
 
 fnm install --lts
-sudo sed -i -E 's/Exec=(.*) mongodb-compass %U$/Exec=\1 mongodb-compass --gtk-version=3 --ignore-additional-command-line-flags --password-store="gnome-libsecret" %U/g' /usr/share/applications/mongodb-compass.desktop
+sudo sed -i -E 's/Exec=mongodb-compass %U$/Exec=mongodb-compass --gtk-version=3 --ignore-additional-command-line-flags --password-store="gnome-libsecret" %U/g' /usr/share/applications/mongodb-compass.desktop
+sudo sed -i -E 's/Exec=(.*) %U/Exec=\1 --gtk-version=3 %U/g' /usr/share/applications/slack.desktop
