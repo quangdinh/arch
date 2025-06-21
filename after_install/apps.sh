a2ad8c81f76e5597cd25bbb213b44acdd56909a8
Quang Dinh Le <qdtc@Carbon.local>
2025-06-21 10:26:45 +0200

    feat: update mongodb install

--- after_install/apps.sh
+++ after_install/apps.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-yay -Syu --noconfirm rbenv ruby-build 1password mongodb-compass-bin \
+yay -Syu --noconfirm rbenv ruby-build 1password \
   postman-bin slack-desktop-wayland google-chrome \
-  gita kubectl k9s fnm-bin oh-my-posh-bin obsidian onlyoffice-bin
+  gita kubectl k9s fnm-bin oh-my-posh-bin obsidian 
+
+yay -S --noconfirm --mflags "--nocheck" mongodb-compass-bin
 
 
 fnm install --lts
-sudo sed -i -E 's/Exec=(.*) mongodb-compass %U$/Exec=\1 mongodb-compass --ignore-additional-command-line-flags --password-store="gnome-libsecret" %U/g' /usr/share/applications/mongodb-compass.desktop
-sudo sed -i -E 's/Exec=(.*)%U/Exec=\1 --gtk-version=3 %U/g' /usr/share/applications/1password.desktop
+sudo sed -i -E 's/Exec=(.*) mongodb-compass %U$/Exec=\1 mongodb-compass --gtk-version=3 --ignore-additional-command-line-flags --password-store="gnome-libsecret" %U/g' /usr/share/applications/mongodb-compass.desktop
