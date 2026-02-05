d101e57c1740fbaca72f2fbd2e0f2d112b7ad090
Quang Dinh Le <lequangdinh@gmail.com>
2026-02-05 13:31:11 +0100

    fix: force makefs

--- install.py
+++ install.py
@@ -329,11 +329,11 @@ def get_cpu_code(cpu):
 
 def format_root(partition, fs):
   if fs == "btrfs":
-    run_command("/usr/bin/mkfs.btrfs", "-L", "ArchRoot", partition)
+    run_command("/usr/bin/mkfs.btrfs", "-L -F", "ArchRoot", partition)
   elif fs == "ext4":
-    run_command("/usr/bin/mkfs.ext4", partition)
+    run_command("/usr/bin/mkfs.ext4", "-F", partition)
   else:
-    run_command("/usr/bin/mkfs.xfs", partition)
+    run_command("/usr/bin/mkfs.xfs", "-F", partition)
 
 cpu = detect_cpu()
 vga = detect_vga()
